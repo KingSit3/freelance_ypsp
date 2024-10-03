755db76f3bf0a36ad5ff4c4fa28b2e315117232c
KingSit3 <i.ardiansyah93@gmail.com>
2024-10-03 12:38:41 +0700

    init commit

---
+++ src/components/ClockComponent.vue
@@ -0,0 +1,178 @@
+<template>
+  <div class="analog-clock">
+    <div class="analog-clock-face">
+      <div class="dots"></div>
+      <div class="dots"></div>
+      <div class="dots"></div>
+      <div class="dots"></div>
+      <div class="dots"></div>
+      <div class="dots"></div>
+      <!-- <div v-bind:style="{ transform: `rotate(${hourDeg}deg)` }" class="hand hour-hand"></div>
+          <div v-bind:style="{ transform: `rotate(${minDeg}deg)` }" class="hand min-hand"></div> -->
+      <div v-bind:style="{ transform: `rotate(${secDeg}deg)` }" class="hand sec-hand"></div>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue'
+
+const secDeg = ref(0)
+// const minDeg = ref(0)
+// const hourDeg = ref(0)
+
+const zeroPadded = (num: any) => {
+  if (num < 10) {
+    return `0${num}`
+  }
+  return num
+}
+
+const clock = () => {
+  let now = new Date()
+  // let hour = zeroPadded(now.getHours())
+  // let min = zeroPadded(now.getMinutes())
+  let sec = zeroPadded(now.getSeconds())
+  secDeg.value = (sec / 60) * 360 + 90
+  // minDeg.value = (min / 60) * 360 + (sec / 60) * 6 + 90
+  // hourDeg.value = (hour / 12) * 360 + (min / 60) * 30 + 90
+}
+const updateClock = () => {
+  setInterval(() => clock(), 1000)
+}
+
+onMounted(() => {
+  updateClock()
+})
+</script>
+
+<style>
+/* ==================
+ *  VARIABLES
+ * ================== */
+:root {
+  --color-black: hsl(0, 0%, 30%);
+  --color-black-light: hsl(0, 0%, 60%);
+  --color-gray: hsl(0, 0%, 90%);
+  --color-white: hsl(0, 0%, 98%);
+
+  --color-red: hsl(348, 100%, 61%);
+  --color-orange: hsl(34, 100%, 50%);
+  --color-green: hsl(123, 80%, 19%);
+
+  --color-yellow-inverse: hsl(48, 100%, 77%);
+  --color-red-inverse: hsl(0, 97%, 34%);
+  --color-orange-inverse: hsl(34, 100%, 70%);
+}
+/* ==================
+ *  BASE
+ * ================== */
+body {
+  font-family: 'Open Sans Condensed', sans-serif;
+  color: var(--color-black);
+}
+.outer-container {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  height: auto;
+}
+/* ==================
+ *  Analog Clock
+ * ================== */
+
+/* *** CLOCK *** */
+
+.analog-clock {
+  border: 10px solid var(--color-green);
+  margin: 0 auto;
+  width: 180px;
+  height: 180px;
+  border-radius: 50%;
+}
+.analog-clock-face {
+  height: 100%;
+  transform: translateY(50%);
+}
+.analog-clock-face::before {
+  content: '';
+  width: 8px;
+  height: 8px;
+  position: absolute;
+  background: white;
+  left: 50%;
+  z-index: 10;
+  transform: translate(-50%, -50%);
+  border: 2px solid var(--color-red-inverse);
+  border-radius: 50%;
+}
+
+/* *** DOTS *** */
+
+.dots:nth-of-type(1) {
+  transform: rotate(0deg);
+}
+.dots:nth-of-type(2) {
+  transform: rotate(90deg);
+}
+.dots:nth-of-type(3) {
+  transform: rotate(30deg);
+}
+.dots:nth-of-type(4) {
+  transform: rotate(60deg);
+}
+.dots:nth-of-type(5) {
+  transform: rotate(120deg);
+}
+.dots:nth-of-type(6) {
+  transform: rotate(150deg);
+}
+.dots::before,
+.dots::after {
+  content: '';
+  position: absolute;
+  background: var(--color-red-inverse);
+  width: 7px;
+  height: 2px;
+  border-radius: 30px;
+  top: -3px;
+}
+.dots::after {
+  left: 7px;
+}
+.dots::before {
+  right: 7px;
+}
+.dots:nth-of-type(1)::before,
+.dots:nth-of-type(1)::after,
+.dots:nth-of-type(2)::before,
+.dots:nth-of-type(2)::after {
+  background: var(--color-red);
+  width: 10px;
+  height: 4px;
+}
+
+/* *** HAND *** */
+
+.hand {
+  background: var(--color-black-light);
+  transform-origin: 100%;
+  border-radius: 10px;
+  position: absolute;
+  right: 50%;
+}
+.hand.hour-hand {
+  width: 25%;
+  height: 5px;
+  background: var(--color-black);
+  z-index: 1;
+}
+.hand.min-hand {
+  width: 37%;
+  height: 4px;
+}
+.hand.sec-hand {
+  background: var(--color-green);
+  width: 39%;
+  height: 2px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,101 @@
+<template>
+  <main class="min-h-screen w-full flex justify-center items-center relative p-20">
+    <div class="absolute top-0 w-full h-full bg-gradient-to-tl from-green-500" />
+    <div
+      class="min-w-[50%] py-10 px-5 gap-5 flex flex-col justify-center items-center z-10 bg-white rounded-xl shadow-2xl"
+    >
+      <div class="relative">
+        <img src="/logo.webp" alt="Logo" class="w-40" />
+        <div class="rotate-180">
+          <ClockComponent />
+        </div>
+        <img src="/blood.svg" alt="Blood" class="w-40 absolute bottom-6 left-7 opacity-95" />
+      </div>
+
+      <!-- Time -->
+      <div class="relative flex flex-col gap-5 w-full">
+        <!-- Minutes & Hours -->
+        <div class="flex justify-around w-full z-10">
+          <div class="flex flex-col items-center">
+            <p class="font-semibold text-lg">Menit</p>
+            <p class="font-extrabold text-3xl">{{ time?.minutes }}</p>
+          </div>
+          <div class="flex flex-col items-center">
+            <p class="font-semibold text-lg">Jam</p>
+            <p class="font-extrabold text-3xl">{{ time?.hours }}</p>
+          </div>
+        </div>
+
+        <div class="w-full h-0.5 bg-black/20" />
+
+        <!-- Weeks, Months & Years -->
+        <div class="flex justify-around w-full z-10">
+          <div class="flex flex-col items-center">
+            <p class="font-semibold text-lg">Minggu</p>
+            <p class="font-extrabold text-3xl">{{ time?.weeks }}</p>
+          </div>
+          <div class="flex flex-col items-center">
+            <p class="font-semibold text-lg">Bulan</p>
+            <p class="font-extrabold text-3xl">{{ time?.months }}</p>
+          </div>
+          <div class="flex flex-col items-center">
+            <p class="font-semibold text-lg">Tahun</p>
+            <p class="font-extrabold text-3xl">{{ time?.years }}</p>
+          </div>
+        </div>
+      </div>
+
+      <p class="font-semibold text-center">Penjajahan dan Agresi sejak 7 Oktober 2023</p>
+      <div class="flex flex-col gap-2 justify-center items-center">
+        <p class="font-semibold">Kontak & Sosial media</p>
+        <div class="flex gap-10">
+          <a href="" class="lg:hover:opacity-50 duration-300">
+            <img src="/telegram-icon.svg" alt="Telegram Icon" />
+          </a>
+          <a href="" class="lg:hover:opacity-50 duration-300">
+            <img src="/whatsapp-icon.svg" alt="Telegram Icon" />
+          </a>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+<script setup lang="ts">
+import ClockComponent from '@/components/ClockComponent.vue'
+import { ref, onMounted } from 'vue'
+
+interface TimeInterface {
+  seconds: string
+  minutes: string
+  hours: string
+  days: string
+  weeks: string
+  months: string
+  years: string
+}
+
+const time = ref<TimeInterface>()
+
+const getDistantDate = () => {
+  const now = new Date()
+  let firstInvasion = new Date('10/07/2023')
+  let diff = now.getTime() - firstInvasion.getTime()
+
+  return {
+    seconds: Math.round(diff / 1000).toString(),
+    minutes: Math.round(diff / (1000 * 60)).toString(),
+    hours: Math.round(diff / (1000 * 3600)).toString(),
+    days: Math.round(diff / (1000 * 3600 * 24)).toString(),
+    weeks: Math.round(diff / (1000 * 3600 * 24 * 7)).toString(),
+    months: Math.round(diff / (1000 * 3600 * 24 * 30)).toString(), // it's not bug, it's feature!
+    years: Math.round(diff / (1000 * 3600 * 24 * 30 * 12)).toString()
+  }
+}
+
+onMounted(() => {
+  time.value = getDistantDate()
+  setInterval(() => {
+    time.value = getDistantDate()
+  }, 1000)
+})
+</script>
